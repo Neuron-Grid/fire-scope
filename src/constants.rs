59335e420aad6ddd259b2be9c4caef3fc7616e88
Neuron Grid <neuron-grid@neuron-grid.net>
2025-04-05 14:24:19 +0900

    定数を共通化しました。

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+/// 定数の共通化
+pub const RIR_URLS: &[&str] = &[
+    "https://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-extended-latest",
+    "https://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest",
+    "https://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-extended-latest",
+    "https://ftp.apnic.net/pub/stats/apnic/delegated-apnic-extended-latest",
+    "https://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest",
+];
